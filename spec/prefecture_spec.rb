e07b5902dc350c12dbefb63bd939a7a76e19d853
chocoby <chocoby@gmail.com>
2013-06-10 00:19:15 +0900

    find メソッドのテストを shared_examples で書き換えた

--- spec/prefecture_spec.rb
+++ spec/prefecture_spec.rb
@@ -10,16 +10,26 @@ describe JpPrefecture::Prefecture do
   end
 
   describe '.find' do
-    context '都道府県が見つかった場合' do
-      let(:pref) { JpPrefecture::Prefecture.find(1) }
+    shared_examples "都道府県が見つかった場合" do
       it { pref.code.should eq 1 }
       it { pref.name.should eq '北海道' }
       it { pref.name_e.should eq 'Hokkaido' }
     end
 
-    context '都道府県が見つからなかった場合' do
-      let(:pref) { JpPrefecture::Prefecture.find(99) }
+    shared_examples '都道府県が見つからなかった場合' do
       it { pref.should be_nil }
+    end
+
+    describe '都道府県コード' do
+      context '都道府県が見つかった場合' do
+        let(:pref) { JpPrefecture::Prefecture.find(1) }
+        it_behaves_like "都道府県が見つかった場合"
+      end
+
+      context '都道府県が見つからなかった場合' do
+        let(:pref) { JpPrefecture::Prefecture.find(99) }
+        it_behaves_like "都道府県が見つからなかった場合"
+      end
     end
   end
 
