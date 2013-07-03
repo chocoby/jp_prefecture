b8cf4208ee013dfacf1b3313b7abebd5077f8e33
mizoR <suzunatsu@yahoo.com>
2013-07-03 13:53:08 +0900

    都道府県情報を再設定可能にした

--- lib/jp_prefecture/mapping.rb
+++ lib/jp_prefecture/mapping.rb
@@ -4,7 +4,12 @@ require 'yaml'
 module JpPrefecture
   # コードと都道府県のマッピング
   module Mapping
-    filepath = File.join File.dirname(__FILE__), '../../data/prefecture.yml'
-    PREFECTURE_CODE_NAME = YAML.load_file filepath
+
+    filepath = File.join(File.dirname(__FILE__), '../../data/prefecture.yml')
+    @data = YAML.load_file(filepath)
+
+    def self.data
+      JpPrefecture.config.mapping_data || @data
+    end
   end
 end
--- spec/jp_prefecture_spec.rb
+++ spec/jp_prefecture_spec.rb
@@ -16,4 +16,21 @@ describe JpPrefecture do
     subject { Class.new(ActiveRecord::Base) }
     it { should_not respond_to(:jp_prefecture) }
   end
+
+  describe '.setup' do
+    before do
+      filepath = File.join(File.dirname(__FILE__), 'fixtures/prefecture.yml')
+      JpPrefecture.setup do |config|
+        config.mapping_data = YAML.load_file filepath
+      end
+    end
+
+    after do
+      JpPrefecture.setup do |config|
+        config.mapping_data = nil
+      end
+    end
+
+    it { JpPrefecture::Mapping.data.count.should eq 48 }
+  end
 end
