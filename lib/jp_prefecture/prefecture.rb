535b271fcb9ca74fe7b8e6896f79a7b113f9f05b
k-motoyan <k.motoyan888@gmail.com>
2016-03-30 12:38:20 +0900

    Appdend gurd to JpPrefecture::Prefecture.find_code_by_name.

--- lib/jp_prefecture/prefecture.rb
+++ lib/jp_prefecture/prefecture.rb
@@ -115,6 +115,9 @@ module JpPrefecture
     # @return [Integer] 見つかった場合は都道府県コード
     # @return [nil] 見つからない場合は nil
     def self.find_code_by_name(name)
+      # nameがnil、空文字の場合は見つからないと判断してnilを返す。
+      return nil if name.nil? || name.empty?
+
       name = name.downcase
 
       Mapping.data.each do |m|
